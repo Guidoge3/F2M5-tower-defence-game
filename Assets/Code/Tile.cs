bee0e20373d06ac1bc37f24069219342c4397398
31888@ma-web.nl <31888@ma-web.nl>
2021-09-17 15:20:23 +0200

    added tiles and prefabs

---
+++ Assets/Code/Tile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField]
+    private bool isBuildable;
+
+    public bool getIsBuildable()
+    {
+        return getIsBuildable();
+    }
+}
